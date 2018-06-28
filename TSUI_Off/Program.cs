e096fa6449ab75c794138060e57bd91ace22e83e
James Geater <jgeater@outlook.com>
2018-06-28 07:59:09 -0400

    initial commit
    sample code for turning off the TSUI in C#

--- TSUI_Off/Program.cs
+++ TSUI_Off/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace TSUI_Off
 {
@@ -6,7 +9,22 @@ namespace TSUI_Off
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sample code to turn off the task sequence UI in a C# application
+            try
+            {
+                Type tsProgress = Type.GetTypeFromProgID("Microsoft.SMS.TSProgressUI");
+                dynamic comObject = Activator.CreateInstance(tsProgress);
+
+                comObject.CloseProgressDialog();
+
+                if (System.Runtime.InteropServices.Marshal.IsComObject(comObject) == true)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(comObject);
+            }
+            catch
+            {
+                Console.WriteLine("Task sequence environment not found!");
+            }
+
         }
     }
 }
