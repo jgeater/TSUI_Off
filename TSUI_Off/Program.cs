8f352196a81b2e6d21960fb5f90111d474527119
James Geater <jgeater@outlook.com>
2018-06-28 07:56:53 -0400

    Add project files.

---
+++ TSUI_Off/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSUI_Off
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
